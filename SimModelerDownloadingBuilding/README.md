32fb9a5890df2f329d24a659b3fd124cc041a730
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2019-04-24 09:31:07 +0200

    explain how to install latest SimModeler

---
+++ SimModelerDownloadingBuilding/README.md
@@ -0,0 +1,32 @@
+# Downloading SimModeler
+
+Go to [the support area of Simmetrix web site](http://www.simmetrix.com/support_area/Support.php) and download the binaries of the code.
+
+# Customizing SimModeler for SeisSol
+Add the files of [this folder](https://github.com/SeisSol/Meshing/tree/master/SimModelerDownloadingBuilding/SimModelerCustomization) to the main directory of SimModeler. These files allow defining properly boundary conditions and exporting the mesh in the proper format.
+
+
+# Downloading SimModeler modeling suite
+
+Go to [the support area of Simmetrix web site](http://www.simmetrix.com/support_area/Support.php) to see the available versions of SimModeler modeling suite. Then use this [script](https://github.com/SeisSol/Meshing/tree/master/SimModelerDownloadingBuilding/downloadSimLib.sh) to download all components of the library. Please make sure that all components that you download are included in your licence agreeement.
+
+# Building SimModeler modeling suite
+First untar all the files, using for instance:
+```bash
+for filename in *.tgz
+do
+  tar zxf $filename
+done
+```
+Then adapt the Makefile in  `code/PartitionWrapper` to your plateform (basically update variables PQUAL (e.g. -mpich2), CC (e.g. mpicc) and CXX (e.g. mpiCC)). As this Makefile usually do not change from one version to the other, it can be wise to copy it to the root folder of your SimModeler installation path, and use this customized file to build any version of the SimModeler library. In this case, the library is then built using:
+`sh compileSimLib.sh <release string ex) 7.1-110613 >` where compileSimLib.sh is this file:
+
+
+```bash
+#!/bin/sh
+cp Makefile $1/code/PartitionWrapper/
+cd  $1/code/PartitionWrapper/
+make PARALLEL=mpich2
+cp lib/libSimPartitionWrapper-*.a ../../lib/x64_rhel6_gcc44/
+cd ../../..
+```
---
+++ SimModelerDownloadingBuilding/downloadSimLib.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -ex
+
+EXPECTED_ARGS=4
+
+if [ $# -ne $EXPECTED_ARGS ];then
+  echo $#
+  echo "Usage: `basename $0` <username> <password> <downloadcode (0 or 1): 0: library 1:documentation><release string ex) 7.1-110613 > "
+  exit 1
+fi;
+
+username=$1
+password=$2
+downloadcode=$3
+release=$4
+
+#is the release a dev release?
+str=$4
+i=$((${#str}-3))
+last3char="${str:$i:3}"
+
+if [ "$last3char" = "dev" ]; then 
+    fold='DM' 
+else
+    fold='M'  
+fi;
+
+# in vi add newlines
+# %s/<\/a>/<\/a>\r/g
+# to source.php( source code of support download site ) then run the command 
+# grep documentation.*zip ~/Downloads/source.php | sed 's/.*documentation\/\([A-Za-z]*.zip\).*/\1/g'
+# to extract the zip file names
+
+
+if [ "$downloadcode" = "1" ]; then 
+    documentation=( GeomSim.zip GeomSimAcis.zip GeomSimDiscrete.zip FieldSim.zip GeomSimAbstract.zip MeshSimCrack.zip MeshSimAdapt.zip MeshSimAdv.zip MeshSim.zip MeshSimCrack.zip ParallelMeshSimAdapt.zip ParallelMeshSim.zip GeomSimProe.zip GeomSimParasolid.zip GeomSimSolidWorks.zip )
+    echo $documentation
+else
+    documentation=()
+fi;
+
+for doc in ${documentation[@]}; do 
+  wget --user=${username} --password=${password} http://www.simmetrix.com/support_area/${fold}/${release}/documentation/${doc}
+done
+
+# run the command
+# grep linux64.tgz ~/Downloads/source.php | sed 's/.*release\/\([a-z]*-linux64.tgz\).*/\1/g'
+# to extract the tarball names
+
+if [ "$downloadcode" = "0" ]; then 
+    components=( gmcore-linux64.tgz aciskrnl-linux64.tgz discrete-linux64.tgz fdcore-linux64.tgz gmabstract-linux64.tgz gmadv-linux64.tgz msadapt-linux64.tgz msadv-linux64.tgz mscore-linux64.tgz msparalleladapt-linux64.tgz msparallelmesh-linux64.tgz pskrnl-linux64.tgz )
+else
+    components=()
+fi;
+
+for comp in ${components[@]}; do 
+  wget --user=${username} --password=${password} http://www.simmetrix.com/support_area/${fold}/${release}/release/${comp}
+done
+
