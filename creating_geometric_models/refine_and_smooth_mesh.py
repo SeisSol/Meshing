7cdbeda2834b4fc01e5063213b1b13dfbf13fe3a
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2021-07-14 09:40:43 +0200

    change default to required in refine_and_smooth

--- creating_geometric_models/refine_and_smooth_mesh.py
+++ creating_geometric_models/refine_and_smooth_mesh.py
@@ -5,14 +5,28 @@ import trimesh
 
 parser = argparse.ArgumentParser(description="refine and smooth")
 parser.add_argument("input_file", help="surface in ts file format")
-parser.add_argument("--N", nargs=1, metavar=("N"), type=int, default=0, help="number of initial refine steps")
-parser.add_argument("--P", nargs=1, metavar=("P"), type=int, default=1, help="number of and refine/smoothing steps")
+parser.add_argument(
+    "--N",
+    nargs=1,
+    metavar=("N"),
+    type=int,
+    required=True,
+    help="number of initial refine steps",
+)
+parser.add_argument(
+    "--P",
+    nargs=1,
+    metavar=("P"),
+    type=int,
+    required=True,
+    help="number of and refine/smoothing steps",
+)
 args = parser.parse_args()
 
 fid = open(args.input_file)
 myFace = Face.from_ts(fid)
 
-a= trimesh.Trimesh(vertices=myFace.vertex , faces=myFace.connect)
+a = trimesh.Trimesh(vertices=myFace.vertex, faces=myFace.connect)
 
 for i in range(args.N[0]):
     a = a.subdivide()
