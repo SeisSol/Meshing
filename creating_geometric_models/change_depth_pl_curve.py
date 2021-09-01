ed1805ebd0719301747e29a7050cdf1e7e27e158
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2021-09-01 09:27:34 +0200

    add python3 shebang

--- creating_geometric_models/change_depth_pl_curve.py
+++ creating_geometric_models/change_depth_pl_curve.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Author: Thomas Ulrich, LMU
 
 # parsing python arguments
--- creating_geometric_models/projTs.py
+++ creating_geometric_models/projTs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 
 parser = argparse.ArgumentParser(description="project ts file")
