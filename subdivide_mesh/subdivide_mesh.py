84e6237cdc11cf368e150b9c671647b98ece8a89
Sebastian Wolf <wolf.sebastian@in.tum.de>
2021-12-16 13:22:36 +0100

    First draft subdivide by four

---
+++ subdivide_mesh/subdivide_mesh.py
@@ -0,0 +1,94 @@
+import seissolxdmf as sx
+import seissolxdmfwriter as sw
+import numpy as np
+
+filename_in = '/work/launch_SeisSol/Helsinki/meshing/subdivide/loh.xdmf'
+filename_out = '/work/launch_SeisSol/Helsinki/meshing/subdivide/test'
+
+def read_xdmf_mesh(filename):
+    mesh = sx.seissolxdmf(filename)
+    geom = mesh.ReadGeometry()
+    connect = mesh.ReadConnect()
+    boundary = mesh.ReadData('boundary')
+    group = mesh.ReadData('group')
+    return geom, connect, boundary, group
+
+def write_xdmf_mesh(filename, geom, connect, boundary, group):
+    sw.write_seissol_output(filename, geom, connect, ["boundary", "group"], [boundary, group], 0.0, [0])
+
+def unpack_boundary(boundary):
+    return np.array([0xFF & (boundary >> (8*i)) for i in range(4)])
+
+def pack_boundary(boundary):
+    return boundary[0] + (boundary[1] << 8) + (boundary[2] << 16) + (boundary[3] << 24)
+
+def subdivide_element(index, geom, connect, boundary, group):
+    element = connect[index,:]
+    vertices = geom[element]
+    a = vertices[0,:]
+    b = vertices[1,:]
+    c = vertices[2,:]
+    d = vertices[3,:]
+    ab = 0.5 * (a + b);
+    ac = 0.5 * (a + c);
+    ad = 0.5 * (a + d);
+    bc = 0.5 * (b + c);
+    bd = 0.5 * (b + d);
+    cd = 0.5 * (c + d);
+    #                    0  1  2  3  4   5   6   7   8   9   
+    new_geom = np.array([a, b, c, d, ab, ac, ad, bc, bd, cd])
+    new_connect = np.array([[0, 4, 5, 6], 
+        [1, 4, 7, 8], 
+        [2, 5, 7, 9], 
+        [3, 6, 8, 9], 
+        [4, 5, 6, 8], 
+        [4, 5, 7, 8], 
+        [5, 6, 8, 9], 
+        [5, 7, 8, 9]])
+
+    original_boundary = unpack_boundary(boundary[index])
+    modified_boundary = np.array([
+        [original_boundary[0], original_boundary[1],                    0, original_boundary[3]],
+        [original_boundary[0], original_boundary[1],                    0, original_boundary[2]],
+        [original_boundary[0], original_boundary[3],                    0, original_boundary[2]],
+        [original_boundary[1], original_boundary[3],                    0, original_boundary[2]],
+        [                   0,                    0,                    0, original_boundary[1]],
+        [original_boundary[0],                    0,                    0,                    0],
+        [                   0, original_boundary[3],                    0,                    0],
+        [                   0,                    0, original_boundary[2],                    0]])
+    
+    new_boundary = np.array([pack_boundary(b) for b in modified_boundary])
+    new_group = np.array([group[index]]*8)
+    return new_geom, new_connect, new_boundary, new_group
+
+geom, connect, boundary, group = read_xdmf_mesh(filename_in)
+
+number_of_elements = connect.shape[0]
+new_geom = np.zeros((10 * number_of_elements, 3), dtype=np.float64)
+new_connect = np.zeros((8 * number_of_elements, 4), dtype=np.uint64)
+new_boundary = np.zeros((8 * number_of_elements,), dtype=np.int32)
+new_group = np.zeros((8 * number_of_elements,), dtype=np.int32)
+
+for i in range(number_of_elements):
+    local_geom, local_connect, local_boundary, local_group = subdivide_element(i, geom, connect, boundary, group)
+
+    new_geom[10*i:10*(i+1),:] = local_geom
+    new_connect[8*i:8*(i+1),:] = local_connect + 10*i
+    new_boundary[8*i:8*(i+1)] = local_boundary.flatten()
+    new_group[8*i:8*(i+1)] = local_group.flatten()
+
+new_geom, inverse = np.unique(new_geom, return_inverse=True, axis=0)
+new_connect = inverse[new_connect]
+
+#filename_out = '/work/launch_SeisSol/Helsinki/meshing/subdivide/triangle'
+#new_geom = np.array([[0.0, 0.0, 0.0],
+#        [1.0, 0.0, 0.0],
+#        [0.0, 1.0, 0.0],
+#        [0.0, 0.0, 1.0]], dtype=np.float64)
+#new_connect = np.array([[0, 1, 2, 3]], dtype=np.uint64)
+#new_boundary = np.array([pack_boundary([1, 2, 3, 4])], dtype=np.int32)
+#new_group = np.array([0.0], dtype=np.int32)
+
+write_xdmf_mesh(filename_out, new_geom, new_connect, new_boundary, new_group)
+
+
