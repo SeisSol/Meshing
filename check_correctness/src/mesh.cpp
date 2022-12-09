7343ecc130f110b9d88f825fdbeee041fa49259f
Sebastian Wolf <wolf.sebastian@in.tum.de>
2022-12-09 09:02:40 +0100

    More improvements

--- check_correctness/src/mesh.cpp
+++ check_correctness/src/mesh.cpp
@@ -13,50 +13,52 @@ Mesh::Mesh(const std::string& fileName) {
   int numPartitionCells = puml.numOriginalCells();
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  //logInfo() << "Keep all" << numPartitionCells << "elements on rank" << rank;
+  // Keep all cells on the same rank
   std::vector<int> partition;
   partition.reserve(numPartitionCells);
   for (size_t elemIdx = 0; elemIdx < numPartitionCells; elemIdx++) {
     partition[elemIdx] = rank;
   }
   puml.partition(partition.data());
-  //logInfo() << "Done.";
-  //logInfo() << "Generate mesh";
   puml.generateMesh();
-  //logInfo() << "Done.";
 }
 
 bool Mesh::checkNeighbors() const {
-  const size_t numElements = this->numElements();
-  for (size_t elemIdx = 0; elemIdx < numElements; elemIdx++) {
+  bool result = true;
+  unsigned int faceids[4]{};
+  int neighbors[4]{};
+  // check all cells
+  for (size_t elemIdx = 0; elemIdx < numElements(); elemIdx++) {
     const auto& cell = puml.cells().at(elemIdx);
-
     const auto bndInfo = puml.cellData(1)[elemIdx];
-    unsigned int faceids[4];
-    PUML::Downward::faces(puml, cell, faceids);
-    int neighbors[4];
-    PUML::Neighbor::face(puml, elemIdx, neighbors);
     auto decodeBC = [&bndInfo](int side) { return (bndInfo >> (side*8)) & 0xFF; };
 
-    //logInfo() << "Element" << elemIdx;
+    PUML::Downward::faces(puml, cell, faceids);
+    PUML::Neighbor::face(puml, elemIdx, neighbors);
+
+    // check all sides
     for (size_t side = 0; side < 4; side++) {
       const auto& face = puml.faces()[faceids[side]];
-      //logInfo() << "side" << side << ":" << decodeBC(side) << ", " << neighbors[side] << face.isShared();
+      const auto sideBC = decodeBC(side);
       // if a face is a regular or a dr face, it has to have a neighbor on either this rank or somewhere else:
-      if (decodeBC(side) == 0 || decodeBC(side) == 3) {
+      if (sideBC == 0 || sideBC == 3) {
         if (neighbors[side] < 0 && !face.isShared()) {
-          logInfo() << "Element" << cell.gid() << ", side" << side << " has a regular or DR boundary condition, but the neighborig element doesn't exist";
-          return false;
+          logInfo() << "Element" << cell.gid() << ", side" << side << " has a" << bcToString(sideBC) << "boundary condition, but the neighborig element doesn't exist";
+          result = false;
         }
       }
       // absorbing or free surface boundaries must not have neighbor elements:
-      else {
+      else if (sideBC == 1 || sideBC == 5) {
         if (neighbors[side] >= 0 || face.isShared()) {
-          logInfo() << "Element" << cell.gid() << ", side" << side << " has an absorbing or free surface boundary conditin, but the neighborig element is not flagged -1";
-          return false;
+          logInfo() << "Element" << cell.gid() << ", side" << side << " has a" << bcToString(sideBC) << "boundary condition, but the neighborig element is not flagged -1";
+          result = false;
         }
+      }
+      // ignore unknown boundary conditions and warn
+      else {
+        logWarning() << "Element" << cell.gid() << ", side" << side << " has a boundary condition, which I don't understand" << sideBC;
       }
     }
   }
-  return true;
+  return result;
 }
--- check_correctness/src/mesh.h
+++ check_correctness/src/mesh.h
@@ -2,21 +2,33 @@
 #define MESH_H_
 
 #include <array>
+#include <map>
 #include <string>
 #include <vector>
 
 #include "PUML/PUML.h"
 
+
 class Mesh {
   public:
   explicit Mesh(std::string const& fileName);
   ~Mesh() = default;
-  [[nodiscard]] unsigned int numVertices() const { return puml.numOriginalVertices(); };
-  [[nodiscard]] unsigned int numElements() const { return puml.numOriginalCells(); };
+  [[nodiscard]] unsigned int numVertices() const { return puml.vertices().size(); };
+  [[nodiscard]] unsigned int numElements() const { return puml.cells().size(); };
   [[nodiscard]] bool checkNeighbors() const;
 
   private:
   PUML::TETPUML puml;
+  std::string_view bcToString(int id) const {
+    switch(id) {
+      case 0: return "regular";
+      case 1: return "free surface";
+      case 3: return "dynamic rupture";
+      case 5: return "absorbing";
+      default: return "";
+    }
+  }
+
 };
 
 #endif
