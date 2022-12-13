f2b7ff3a3ffd66109cdfe8ab72c0741bfa960e24
Sebastian Wolf <24592027+sebwolf-de@users.noreply.github.com>
2022-12-13 09:23:42 +0100

    Small script which checks, whether a mesh is correct. (#42)
    * Small script which checks, whether a mesh is correct.
    
    * Some improvements
    
    * Add readme
    
    * More improvements
    
    * Fix strange PUML behaviour about `gid`
    
    * Consider all possible SeisSol BCs
    
    * Rename -> check_mesh
    
    * Consider Review
    
    * Fix typos
    
    * Correctly fill cellIdsAsInFile
    
    * Add free surface with gravity

---
+++ check_mesh/README.md
@@ -0,0 +1,34 @@
+# Evaluate material model on mesh
+
+## Compilation
+
+```
+mkdir build && cd build
+cmake ..
+make
+```
+
+## Usage
+
+```
+mpirun -n 4 ./check_mesh -m mesh.h5
+```
+
+## Description
+
+1. Read `mesh.h5`
+2. For each element check that:
+  - if a face is an internal face, the neighbor *has to exist*.
+  - if a face is an external face, the neighbor *must not exist*.
+3. If an element breaks above rules, the program writes an error message.
+4. If all elements are correct, the programm exits with a success output. If at least one element is broken, the program returns with an error code.
+
+Internal faces are:
+* Regular
+* Dynamic Rupture
+* Dynamic Rupture with Fault Tagging
+External faces are:
+* Free Surface
+* Absorbing
+* Periodic
+* Free Surface with Gravity
---
+++ check_mesh/src/mesh.h
@@ -0,0 +1,54 @@
+#ifndef MESH_H_
+#define MESH_H_
+
+#include <array>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "PUML/PUML.h"
+
+
+class Mesh {
+  public:
+  explicit Mesh(std::string const& fileName);
+  ~Mesh() = default;
+  [[nodiscard]] std::size_t numVertices() const { return puml.vertices().size(); }
+  [[nodiscard]] std::size_t numElements() const { return puml.cells().size(); }
+  [[nodiscard]] bool checkNeighbors() const;
+
+  private:
+  PUML::TETPUML puml;
+
+  enum class BCType {
+    internal, external, unknown
+  };
+
+  BCType bcToType(int id) const {
+    if (id == 0 || id == 3 || id > 64) {
+      return BCType::internal;
+    } else if (id == 1 || id == 5 || id == 6 || id == 7) {
+      return BCType::external;
+    } else {
+      return BCType::unknown;
+    }
+  }
+
+  std::string bcToString(int id) const {
+    if (id == 0) { return std::string("regular"); }
+    else if (id == 1) { return std::string("free surface"); }
+    else if (id == 3) { return std::string("dynamic rupture"); }
+    else if (id == 5) { return std::string("absorbing"); }
+    else if (id == 6) { return std::string("periodic"); }
+    else if (id == 6) { return std::string("free surface with gravity"); }
+    else if (id > 64) { 
+      std::stringstream s;
+      s << "fault-tagging (" << id << ")";
+      return s.str();
+    } else {return std::string(""); }
+  }
+
+};
+
+#endif
